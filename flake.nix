c5937e4f7c693da98f76694f71567bd1196417aa
Mistivia <i@mistivia.com>
2026-03-16 20:23:17 +0800

    add deps

--- flake.nix
+++ flake.nix
@@ -4,13 +4,28 @@
     inputs = {
         nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
         flake-utils.url = "github:numtide/flake-utils";
+        flex-record = {
+            url = "github:mistivia/flex-record";
+            inputs.nixpkgs.follows = "nixpkgs";
+            inputs.flake-utils.follows = "flake-utils";
+        };
+        accessor-hs = {
+            url = "github:mistivia/accessor-hs";
+            inputs.nixpkgs.follows = "nixpkgs";
+            inputs.flake-utils.follows = "flake-utils";
+        };
     };
 
-    outputs = { self, nixpkgs, flake-utils }:
+    outputs = { self, nixpkgs, flake-utils, flex-record, accessor-hs }:
         flake-utils.lib.eachDefaultSystem (system:
         let
             pkgs = nixpkgs.legacyPackages.${system};
-            haskellPackages = pkgs.haskellPackages;
+            haskellPackages = pkgs.haskellPackages.override {
+                overrides = hself: hsuper: {
+                    "flex-record" = flex-record.packages.${system}.default;
+                    "accessor-hs" = accessor-hs.packages.${system}.default;
+                };
+            };
             project = haskellPackages.callCabal2nix "typed-rpc" ./. {};
             devTools = with haskellPackages; [
                 cabal-install
